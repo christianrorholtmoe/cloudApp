6f530b213ab167abb86a366d159070921860e7b3
christianrorholtmoe <34798018+christianrorholtmoe@users.noreply.github.com>
2018-09-24 20:12:08 +0000

    First Spring Boot commit

--- cloudapp/src/main/java/azure532/cloudapp/App.java
+++ cloudapp/src/main/java/azure532/cloudapp/App.java
@@ -1,13 +1,20 @@
 package azure532.cloudapp;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hei Azure!" );
-    }
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@EnableAutoConfiguration
+public class App {
+
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(App.class, args);
+	}
+
 }
