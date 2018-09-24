9e7f844138e1d398a65c7d2e2e219c409f6ee4fc
christianrorholtmoe <34798018+christianrorholtmoe@users.noreply.github.com>
2018-09-24 19:55:23 +0000

    Jepp, the master

---
+++ cloudapp/src/main/java/azure532/cloudapp/App.java
@@ -0,0 +1,13 @@
+package azure532.cloudapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hei Azure!" );
+    }
+}
